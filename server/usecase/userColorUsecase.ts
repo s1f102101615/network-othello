ae7cc368761c10e8b832fd85b1b1b768907620bd
s1f102101615 <s1f102101615@iniad.org>
2023-07-04 20:49:26 +0900

    userColorUsecaseのlint回避

--- server/usecase/userColorUsecase.ts
+++ server/usecase/userColorUsecase.ts
@@ -1,14 +1,9 @@
-/* eslint-disable complexity */
 import type { UserId } from '$/commonTypesWithClient/branded';
 import { roomsRepository } from '$/repository/roomsRepository';
 import assert from 'assert';
 
 export const userColorUsecase = {
-  getUserColor: async (
-    userID: UserId,
-    RoomId: string | string[] | undefined
-    // userDisplayName: string | undefined
-  ): Promise<number> => {
+  getUserColor: async (userID: UserId, RoomId: string | string[] | undefined): Promise<number> => {
     const latest = await roomsRepository.findLatest();
     assert(latest, 'クリック出来てるんだからRoomが無いわけがない');
     const rooms = latest.find((room) => room.id === RoomId);
@@ -22,24 +17,10 @@ export const userColorUsecase = {
     } else if (rooms.watcher?.includes(userID)) {
       console.log('3');
       return 3;
-    } else if (rooms.black === 'undefined') {
-      console.log('4');
-      // rooms.black = userID;
-      // rooms.blackname = userDisplayName;
-      // await roomsRepository.save(rooms);
-      return 1;
-    } else if (rooms.white === 'undefined') {
-      console.log('5');
-      // rooms.white = userID;
-      // rooms.whitename = userDisplayName;
-      // rooms.status = 'playing';
-      await roomsRepository.save(rooms);
-      return 2;
-    } else {
-      console.log('6');
-      rooms.watcher?.push(userID);
-      await roomsRepository.save(rooms);
-      return 3;
     }
+    console.log('6');
+    rooms.watcher?.push(userID);
+    await roomsRepository.save(rooms);
+    return 3;
   },
 };
